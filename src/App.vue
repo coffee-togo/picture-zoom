f62cf92719cd18b4ff04e95f907bd80eeb9600df
coffee-togo <111367211+coffee-togo@users.noreply.github.com>
2022-08-16 22:57:35 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <div>
+      <div class="wrapper-image">
+        <pic-zoom :size="size" :src="bg"/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import picZoom from './components/picture-zoom'
+import Bg from './assets/background.jpg'
+
+export default {
+  name: 'App',
+  data () {
+    return {
+      bg: Bg,
+      size: {
+        width: '200',
+        height: '200'
+      }
+    }
+  },
+  components: {
+    picZoom
+  }
+}
+</script>
+
+<style lang="scss">
+    .wrapper-image {
+      width: 400px !important;
+      height: 400px !important;
+
+      border: 1px solid #ececec;
+      border-radius: 4px;
+      img {
+        object-fit: contain; // CSS图片保持原比例
+      }
+    }
+    * {
+      margin: 0;
+      padding: 0;
+    }
+</style>
